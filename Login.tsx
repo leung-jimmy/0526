4098cf5ba9ab2bf49ea2940e120db29e4899d0e7
耿敏捷 <gengminjie@gengminjiedeMacBook-Pro.local>
2025-05-27 10:15:44 +0800

    add login

--- Login.tsx
+++ Login.tsx
@@ -1,15 +1,18 @@
 import React, { FC } from 'react'
 import { useNavigate } from 'react-router-dom'
 
+// 哈哈哈哈
+
 const Login: FC = () => {
   const nav=useNavigate()
   function clickHandler(){
     nav(-1)
   }
+// 这段话在这里
   return (
     <div>
       <div>Login</div>
-      <button onClick={clickHandler}>返回</button>
+      <button onClick={clickHandler}>返回了么？不想返回</button>
     </div>
   )
 }
